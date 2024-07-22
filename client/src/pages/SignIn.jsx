78deed3b053c40f34b9c2f61cebae982d2cc74de
Samridhi Wadhwa <wadhwasamridhi04@gmail.com>
2024-07-22 14:08:50 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,23 +1,23 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link,useNavigate} from "react-router-dom";
-
+import { signInStart,signInsuccess,signInFailure } from "../redux/user/userSlice";
+import { useDispatch,useSelector } from "react-redux";
 const SignIn = () => {
     const [formData, setFormData] =useState({});
-    const [errorMessage,setErrorMessage] = useState(null);
-    const [loading,setLoading] = useState(false);
+    const {loading,error:errorMessage}=useSelector(state=>state.user)
+    const dispatch =useDispatch();
     const navigate = useNavigate();
     const handleChange=(e)=>{
         setFormData({...formData,[e.target.id]:e.target.value.trim()});
     }
     const handleSubmit= async (e)=>{
         if(!formData.email||!formData.password){
-            return setErrorMessage('Please fill out all the feilds');
+            return dispatch(signInFailure('Please fill out all the feilds'))
         }
         e.preventDefault();
         try {
-            setLoading(true);
-            setErrorMessage(null);
+            dispatch(signInStart())
             const res = await fetch('http://localhost:3000/api/auth/signin',{
                 method:'POST',
                 headers:{'Content-Type':'application/json'},
@@ -25,16 +25,17 @@ const SignIn = () => {
             })
             const data = await res.json();
             if(data.success ===false){
-                return setErrorMessage(data.message)
+                dispatch(signInFailure(data.message))
             }
-            setLoading(false);
+            
+
             if(res.ok){
+                dispatch(signInsuccess(data))
                 navigate('/')
             }
         } catch (error) {
             
-            setErrorMessage(error.message);
-            setLoading(false);
+           dispatch(signInFailure(error.message))
             
         }
     }
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+    currentUser:null,
+    error:null, 
+    loading:false
+}
+
+const userSlice = createSlice({
+    name:"user",
+    initialState,
+    reducers:{
+        signInStart :(state)=>{
+            state.loading=false;
+            state.error=null;
+        },
+        signInsuccess:(state,action)=>{
+            state.currentUser = action.payload;
+            state.loading=false;
+            state.action=null;
+        },
+        signInFailure :(state,action)=>{
+            state.loading=false;
+            state.error=action.payload;
+        }
+    }
+})
+
+export const {signInStart,signInsuccess,signInFailure} =userSlice.actions;
+export default userSlice.reducer;
